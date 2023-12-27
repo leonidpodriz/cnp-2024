32924623ed0cf0f49826514a8b79ed1070eff3a3
Leonid Podriz <leonidpodriz@pm.me>
2023-12-28 00:24:34 +0200

    setup webpack

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/*.{html,js}"],
+    content: ['./src/*.html'],
     theme: {
-        extend: {
-            colors: {
-                'gradient-start': '#7B02A3',
-                'gradient-end': '#E40255'
-            },
-            transitionProperty: {
-                'height': 'height'
-            }
-        }
+        extend: {},
+    },
+    variants: {
+        extend: {},
     },
     plugins: [],
 }
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/script.js',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(html)$/i,
+                use: ['html-loader'],
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                dependency: { not: ['url'] },
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+    ],
+    devServer: {
+        client: {
+            progress: true,
+        },
+    }
+}
